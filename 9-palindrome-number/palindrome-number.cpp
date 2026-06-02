2a591c742050a69c7fc762ab719983eb68ef2f6a
Vihanp24 <vihanpurohit088@gmail.com>
2026-06-02 11:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (65.33%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int original =x;
+        long long res =0;
+        while(x>0){
+            int ld =x%10;
+            res = res*10 + ld;
+            x = x/10;
+        }
+        if(original == res){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
